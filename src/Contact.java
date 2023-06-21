c57834d5c7e48c5d282fd04e3acba9e08608e68c
Geree <smithzero94@gmail.com>
2023-06-21 19:41:34 +0200

    Contact class implemented

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,18 @@
 public class Contact {
     // TODO vegyél fel egy név és egy telefonszám fieldet, írj konstruktort és gettereket!
+    private final String name;
+    private final String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
